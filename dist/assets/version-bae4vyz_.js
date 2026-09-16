7abb93496ee14405d25c3745e6de0cb60fed15e6
Lipari Matias Ariel <matylipsa@gmail.com>
2026-09-16 02:44:58 -0300

    Updated index.html

--- dist/assets/version-bae4vyz_.js
+++ dist/assets/version-bae4vyz_.js
@@ -1,4 +1,4 @@
-import{a as zp,c as Dt,g as Vp}from"./index-C6PcgUHo.js";function Wp(t,e){for(var n=0;n<e.length;n++){const s=e[n];if(typeof s!="string"&&!Array.isArray(s)){for(const r in s)if(r!=="default"&&!(r in t)){const a=Object.getOwnPropertyDescriptor(s,r);a&&Object.defineProperty(t,r,a.get?a:{enumerable:!0,get:()=>s[r]})}}}return Object.freeze(Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}))}/**
+import{a as zp,c as Dt,g as Vp}from"./index-DozEBPGO.js";function Wp(t,e){for(var n=0;n<e.length;n++){const s=e[n];if(typeof s!="string"&&!Array.isArray(s)){for(const r in s)if(r!=="default"&&!(r in t)){const a=Object.getOwnPropertyDescriptor(s,r);a&&Object.defineProperty(t,r,a.get?a:{enumerable:!0,get:()=>s[r]})}}}return Object.freeze(Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}))}/**
  * @license
  * Copyright 2020 Google LLC. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
